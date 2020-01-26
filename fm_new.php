75cc121d546e85bc33bdcb88f24aa91a1dccfb60
Yehuda Eisenberg <32451776+YehudaEi@users.noreply.github.com>
2020-01-26 14:57:41 +0200

    minor bugs

--- fm_new.php
+++ fm_new.php
@@ -315,10 +315,10 @@ if (!@is_dir($fileManagerConfig['root_path'])) {
 defined('FM_SHOW_HIDDEN') || define('FM_SHOW_HIDDEN', $show_hidden_files);
 defined('FM_ROOT_PATH') || define('FM_ROOT_PATH', $fileManagerConfig['root_path']);
 defined('FM_LANG') || define('FM_LANG', $lang);
-defined('FM_FILE_EXTENSION') || define('FM_FILE_EXTENSION', $allowed_file_extensions);
-defined('FM_UPLOAD_EXTENSION') || define('FM_UPLOAD_EXTENSION', $allowed_upload_extensions);
-defined('FM_EXCLUDE_ITEMS') || define('FM_EXCLUDE_ITEMS', $exclude_items);
-defined('FM_DOC_VIEWER') || define('FM_DOC_VIEWER', $online_viewer);
+defined('FM_FILE_EXTENSION') || define('FM_FILE_EXTENSION', $fileManagerConfig['allowed_file_extensions']);
+defined('FM_UPLOAD_EXTENSION') || define('FM_UPLOAD_EXTENSION', $fileManagerConfig['allowed_upload_extensions']);
+defined('FM_EXCLUDE_ITEMS') || define('FM_EXCLUDE_ITEMS', $fileManagerConfig['exclude_items']);
+defined('FM_DOC_VIEWER') || define('FM_DOC_VIEWER', $fileManagerConfig['online_viewer']);
 define('FM_READONLY', $fileManagerConfig['use_auth'] && !empty($readonly_users) && isset($_SESSION[FM_SESSION_ID]['logged']) && in_array($_SESSION[FM_SESSION_ID]['logged'], $readonly_users));
 define('FM_IS_WIN', DIRECTORY_SEPARATOR == '\\');
 
