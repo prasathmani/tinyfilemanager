c741ae2212a2a8f8b092cd6592dbbedd7e9d88ba
Michael Milette <michael.milette@tngconsulting.ca>
2020-06-27 06:32:09 -0400

    Notice: A non well formed numeric value encountered (#386)
    Eliminates the following PHP warnings when error reporting is turned on:
    
    Notice: A non well formed numeric value encountered in tinyfilemanager.php on line 2443
    Notice: A non well formed numeric value encountered in tinyfilemanager.php on line 2444
    
    This happens because PHP floor and round are expecting a (float), not an (int).

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -2439,6 +2439,7 @@ function fm_get_size($file)
  */
 function fm_get_filesize($size)
 {
+    $size = (float) $size;
     $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
     $power = $size > 0 ? floor(log($size, 1024)) : 0;
     return sprintf('%s %s', round($size / pow(1024, $power), 2), $units[$power]);
