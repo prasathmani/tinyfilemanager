3c7a04ca0c0e1ba7bd24a4eb267927edf9d63dec
Sharif El Shobkshy <sharif.elshobkshy@gmail.com>
2021-01-04 14:54:41 -0300

    Set correct FM_SELF_URL

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -232,7 +232,7 @@ $root_url = fm_clean_path($root_url);
 defined('FM_ROOT_URL') || define('FM_ROOT_URL', ($is_https ? 'https' : 'http') . '://' . $http_host . (!empty($root_url) ? '/' . $root_url : ''));
 defined('FM_SELF_URL') || define('FM_SELF_URL', ($is_https ? 'https' : 'http') . '://' . $http_host . $_SERVER['REDIRECT_URL']);
 
-var_dump($_SERVER['REDIRECT_URL']);
+print_r($_SERVER);
 
 
 // logout
