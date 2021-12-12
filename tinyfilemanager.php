b884ec94f2a18d0e35222cc58f20361c54562822
Suyadi <suyadi.1992@gmail.com>
2021-12-13 00:57:34 +0700

    Fix a non well formatted numeric value encountered & Notice: Undefined offset in `fm_get_filesize()`
    For non `well formatted numeric value encountered`, see #670
    For `Notice: Undefined offset 9` (current implementation result) see: https://3v4l.org/1qHit#v5.0.0
    For new impementation result, see: https://3v4l.org/d0UBh#v5.0.0

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -2501,7 +2501,8 @@ function fm_get_filesize($size)
 {
     $size = (float) $size;
     $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
-    $power = $size > 0 ? floor(log($size, 1024)) : 0;
+    $power = ($size > 0) ? floor(log($size, 1024)) : 0;
+    $power = ($power > (count($units) - 1)) ? (count($units) - 1) : $power;
     return sprintf('%s %s', round($size / pow(1024, $power), 2), $units[$power]);
 }
 
