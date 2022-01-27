235b1ab87c4086292f72f131d710a32f8a3db14b
Enyedi Szilárd <Szilard.Enyedi@gmail.com>
2022-01-28 00:47:27 +0200

    Update tinyfilemanager.php
    Changed "Folder is empty" message to be language-specific.

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -2062,7 +2062,7 @@ $tableTheme = (FM_THEME == "dark") ? "text-white bg-dark table-dark" : "bg-white
                 <tfoot>
                     <tr><?php if (!FM_READONLY): ?>
                             <td></td><?php endif; ?>
-                        <td colspan="<?php echo (!FM_IS_WIN && !$hide_Cols) ? '6' : '4' ?>"><em><?php echo 'Folder is empty' ?></em></td>
+                        <td colspan="<?php echo (!FM_IS_WIN && !$hide_Cols) ? '6' : '4' ?>"><em><?php echo lng('Folder is empty') ?></em></td>
                     </tr>
                 </tfoot>
                 <?php
