a0d313a90bce25dba7363fad9f73bc533f4b6162
Alice Jacka <jacka.alice@gmail.com>
2022-05-31 21:44:54 +1000

    correct $hide_directLink default value
    I think it's time for bed

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -80,7 +80,7 @@ $iconv_input_encoding = 'UTF-8';
 $datetime_format = 'd.m.y H:i';
 
 // hide the direct link button in the actions collumn of file list
-$hide_directLink = true;
+$hide_directLink = false;
 
 // Allowed file extensions for create and rename files
 // e.g. 'txt,html,css,js'
@@ -2054,7 +2054,7 @@ $tableTheme = (FM_THEME == "dark") ? "text-white bg-dark table-dark" : "bg-white
                             <a title="<?php echo lng('Rename')?>" href="#" onclick="rename('<?php echo fm_enc(addslashes(FM_PATH)) ?>', '<?php echo fm_enc(addslashes($f)) ?>');return false;"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
                             <a title="<?php echo lng('CopyTo')?>..." href="?p=&amp;copy=<?php echo urlencode(trim(FM_PATH . '/' . $f, '/')) ?>"><i class="fa fa-files-o" aria-hidden="true"></i></a>
                         <?php endif;
-                        if ($hide_directLink) {?>
+                        if (!$hide_directLink) {?>
                         <a title="<?php echo lng('DirectLink')?>" href="<?php echo fm_enc(FM_ROOT_URL . (FM_PATH != '' ? '/' . FM_PATH : '') . '/' . $f . '/') ?>" target="_blank"><i class="fa fa-link" aria-hidden="true"></i></a>
                         <?php } ?>
                     </td>
@@ -2121,7 +2121,7 @@ $tableTheme = (FM_THEME == "dark") ? "text-white bg-dark table-dark" : "bg-white
                             <a title="<?php echo lng('CopyTo') ?>..."
                                href="?p=<?php echo urlencode(FM_PATH) ?>&amp;copy=<?php echo urlencode(trim(FM_PATH . '/' . $f, '/')) ?>"><i class="fa fa-files-o"></i></a>
                         <?php endif; 
-                        if ($hide_directLink) {?>
+                        if (!$hide_directLink) {?>
                         <a title="<?php echo lng('DirectLink') ?>" href="<?php echo fm_enc(FM_ROOT_URL . (FM_PATH != '' ? '/' . FM_PATH : '') . '/' . $f) ?>" target="_blank"><i class="fa fa-link"></i></a>
                         <?php } ?>
                         <a title="<?php echo lng('Download') ?>" href="?p=<?php echo urlencode(FM_PATH) ?>&amp;dl=<?php echo urlencode($f) ?>"><i class="fa fa-download"></i></a>
