4d8d4a3aba4cdc23214a5da9e6b94c9160afb36c
Prasath Mani <prasathmani@users.noreply.github.com>
2022-11-07 22:48:15 +0530

    Update tinyfilemanager.php

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -1,6 +1,6 @@
 <?php
 //Default Configuration
-$CONFIG = '{"lang":"en","error_reporting":true,"show_hidden":false,"hide_Cols":false,"calc_folder":false,"theme":"light"}';
+$CONFIG = '{"lang":"en","error_reporting":false,"show_hidden":false,"hide_Cols":false,"calc_folder":false,"theme":"light"}';
 
 
 /**
