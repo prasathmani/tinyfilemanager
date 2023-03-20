fd495ce6471f0c0d04f2fe7349b1e4a8a0de6511
Joy Biswas <joybiswas040701@gmail.com>
2023-03-20 20:56:49 +0600

    replaced array() with []

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -1,6 +1,6 @@
 <?php
 //Default Configuration
-$CONFIG = '{"lang":"bn","error_reporting":false,"show_hidden":false,"hide_Cols":false,"theme":"dark"}';
+$CONFIG = '{"lang":"en","error_reporting":false,"show_hidden":false,"hide_Cols":false,"theme":"light"}';
 
 /**
  * H3K | Tiny File Manager V2.5.3
