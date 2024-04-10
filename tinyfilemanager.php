6fea129832770c26e3aba3f644bcee1f457c18d5
Jerome deville <jerome@deville-net.fr>
2024-04-10 13:09:04 +0200

    Update tinyfilemanager.php
    Put default configuration again

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -1,6 +1,6 @@
 <?php
 //Default Configuration
-$CONFIG = '{"lang":"fr","error_reporting":false,"show_hidden":false,"hide_Cols":false,"theme":"light"}';
+$CONFIG = '{"lang":"en","error_reporting":false,"show_hidden":false,"hide_Cols":false,"theme":"light"}';
 
 /**
  * H3K | Tiny File Manager V2.5.3
@@ -27,13 +27,14 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'HubFonctionnement' => '$2y$10$cmuylHc.mH7.ycVxC1SrjOAu/FCTZZGHA.JTgMFr50Z95pE8PbGiG'
+    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
+    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
 );
 
 // Readonly users
 // e.g. array('users', 'guest', ...)
 $readonly_users = array(
-    // 'user'
+    'user'
 );
 
 // Global readonly, including when auth is not being used
