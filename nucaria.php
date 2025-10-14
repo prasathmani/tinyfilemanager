2252f2751e48714d370ee0685bfd217cb1e5fb13
kevin noguera <kevin5631noguera@gmail.com>
2025-10-14 01:42:43 -0400

    new file:   .gitignore 	new file:   commands.txt 	renamed:    tinyfilemanager.php -> nucaria.php 	modified:   translation.json

--- nucaria.php
+++ nucaria.php
@@ -3,9 +3,9 @@
 $CONFIG = '{"lang":"en","error_reporting":false,"show_hidden":false,"hide_Cols":false,"theme":"light"}';
 
 /**
- * H3K ~ Tiny File Manager V2.6
+ * H3K ~ Nvcaria Beta 
  * @author CCP Programmers
- * @github https://github.com/prasathmani/tinyfilemanager
+ * @github https://github.com/KevinN2025/nvcaria
  * @link https://tinyfilemanager.github.io
  */
 
@@ -13,7 +13,7 @@ $CONFIG = '{"lang":"en","error_reporting":false,"show_hidden":false,"hide_Cols":
 define('VERSION', '2.6');
 
 //Application Title
-define('APP_TITLE', 'Tiny File Manager');
+define('APP_TITLE', 'Nnvaria File Manager ');
 
 // --- EDIT BELOW CONFIGURATION CAREFULLY ---
 
@@ -24,7 +24,6 @@ $use_auth = true;
 
 // Login user name and password
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
-// Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
     'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
     'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
