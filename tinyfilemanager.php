0e8069e24f877ab2fdc711b27425b738d04fac84
maghuro <mrasquinho@gmail.com>
2025-12-05 19:25:41 +0000

    Singular vs plural fix
    And add translations in pt-PT

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -684,10 +684,10 @@ if (isset($_GET['del'], $_POST['token']) && !FM_READONLY) {
         }
         $is_dir = is_dir($path . '/' . $del);
         if (fm_rdelete($path . '/' . $del)) {
-            $msg = $is_dir ? lng('Folder') . ' <b>%s</b> ' . lng('Deleted') : lng('File') . ' <b>%s</b> ' . lng('Deleted');
+            $msg = $is_dir ? lng('Folder') . ' <b>%s</b> ' . lng('Deleted') : mb_strtolower(lng('File'), 'UTF-8') . ' <b>%s</b> ' . lng('Deleted');
             fm_set_msg(sprintf($msg, fm_enc($del)));
         } else {
-            $msg = $is_dir ? lng('Folder') . ' <b>%s</b> ' . lng('not deleted') : lng('File') . ' <b>%s</b> ' . lng('not deleted');
+            $msg = $is_dir ? lng('Folder') . ' <b>%s</b> ' . lng('not deleted') : mb_strtolower(lng('File'), 'UTF-8') . ' <b>%s</b> ' . lng('not deleted');
             fm_set_msg(sprintf($msg, fm_enc($del)), 'error');
         }
     } else {
@@ -1828,7 +1828,13 @@ if (isset($_GET['view'])) {
                   <a class="fw-bold btn btn-outline-primary"
                      title="<?php echo lng('Delete'); ?>"
                      href="?p=<?php echo urlencode(FM_PATH); ?>&amp;del=<?php echo urlencode($file); ?>"
-                     onclick="confirmDialog(event, 1209, '<?php echo lng('Delete') . ' ' . lng('File'); ?>', <?php echo json_encode($file); ?>, this.href);">
+                     onclick="confirmDialog(
+                              event,
+                              1209,
+                              '<?php echo mb_strtolower(lng('Delete') . ' ' . lng('File'), 'UTF-8'); ?>',
+                              <?php echo json_encode($file); ?>,
+                              this.href
+                          );"
                      <i class="fa fa-trash"></i> <?php echo lng('Delete'); ?>
                   </a>
                 <?php endif; ?>
@@ -2218,7 +2224,17 @@ $all_files_size = 0;
                         </td>
                     <?php endif; ?>
                     <td class="inline-actions"><?php if (!FM_READONLY): ?>
-                            <a title="<?php echo lng('Delete') ?>" href="?p=<?php echo urlencode(FM_PATH) ?>&amp;del=<?php echo urlencode($f) ?>" onclick="confirmDialog(event, '1028','<?php echo lng('Delete') . ' ' . lng('Folder'); ?>','<?php echo urlencode($f) ?>', this.href);"> <i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                            <a title="<?php echo lng('Delete'); ?>"
+                                      href="?p=<?php echo urlencode(FM_PATH); ?>&amp;del=<?php echo urlencode($f); ?>"
+                                      onclick="confirmDialog(
+                                       event,
+                                       '1028',
+                                       '<?php echo mb_strtolower(lng('Delete') . ' ' . lng('Folder'), 'UTF-8'); ?>',
+                                       '<?php echo urlencode($f); ?>',
+                                       this.href
+                                      );">
+                                      <i class="fa fa-trash-o" aria-hidden="true"></i>
+                            </a>
                             <a title="<?php echo lng('Rename') ?>" href="#" onclick="rename('<?php echo fm_enc(addslashes(FM_PATH)) ?>', '<?php echo fm_enc(addslashes($f)) ?>');return false;"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
                             <a title="<?php echo lng('CopyTo') ?>..." href="?p=&amp;copy=<?php echo urlencode(trim(FM_PATH . '/' . $f, '/')) ?>"><i class="fa fa-files-o" aria-hidden="true"></i></a>
                         <?php endif; ?>
@@ -2295,13 +2311,34 @@ $all_files_size = 0;
                     <?php endif; ?>
                     <td class="inline-actions">
                         <?php if (!FM_READONLY): ?>
-                            <a title="<?php echo lng('Delete') ?>" href="?p=<?php echo urlencode(FM_PATH) ?>&amp;del=<?php echo urlencode($f) ?>" onclick="confirmDialog(event, 1209, '<?php echo lng('Delete') . ' ' . lng('File'); ?>','<?php echo urlencode($f); ?>', this.href);"> <i class="fa fa-trash-o"></i></a>
+                          <?php $encoded = urlencode($f); ?>
+                            <a title="<?php echo lng('Delete'); ?>"
+                                       href="?p=<?php echo urlencode(FM_PATH); ?>&amp;del=<?php echo $encoded; ?>"
+                                       onclick="confirmDialog(
+                                           event,
+                                           1209,
+                                           '<?php echo mb_strtolower(lng('Delete') . ' ' . lng('File'), 'UTF-8'); ?>',
+                                           '<?php echo $encoded; ?>',
+                                           this.href
+                                       );">
+                                        <i class="fa fa-trash-o"></i>
+                            </a>
                             <a title="<?php echo lng('Rename') ?>" href="#" onclick="rename('<?php echo fm_enc(addslashes(FM_PATH)) ?>', '<?php echo fm_enc(addslashes($f)) ?>');return false;"><i class="fa fa-pencil-square-o"></i></a>
                             <a title="<?php echo lng('CopyTo') ?>..."
                                 href="?p=<?php echo urlencode(FM_PATH) ?>&amp;copy=<?php echo urlencode(trim(FM_PATH . '/' . $f, '/')) ?>"><i class="fa fa-files-o"></i></a>
                         <?php endif; ?>
                         <a title="<?php echo lng('DirectLink') ?>" href="<?php echo fm_enc(FM_ROOT_URL . (FM_PATH != '' ? '/' . FM_PATH : '') . '/' . $f) ?>" target="_blank"><i class="fa fa-link"></i></a>
-                        <a title="<?php echo lng('Download') ?>" href="?p=<?php echo urlencode(FM_PATH) ?>&amp;dl=<?php echo urlencode($f) ?>" onclick="confirmDialog(event, 1211, '<?php echo lng('Download'); ?>','<?php echo urlencode($f); ?>', this.href);"><i class="fa fa-download"></i></a>
+                        <a title="<?php echo lng('Download'); ?>"
+                                   href="?p=<?php echo urlencode(FM_PATH); ?>&amp;dl=<?php echo urlencode($f); ?>"
+                                   onclick="confirmDialog(
+                                       event,
+                                       1211,
+                                       '<?php echo mb_strtolower(lng('Download'), 'UTF-8'); ?>',
+                                       '<?php echo urlencode($f); ?>',
+                                       this.href
+                                   );">
+                                    <i class="fa fa-download"></i>
+                        </a>
                     </td>
                 </tr>
             <?php
@@ -2322,8 +2359,8 @@ $all_files_size = 0;
                     <tr>
                         <td class="gray fs-7" colspan="<?php echo (!FM_IS_WIN && !$hide_Cols) ? (FM_READONLY ? '6' : '7') : (FM_READONLY ? '4' : '5') ?>">
                             <?php echo lng('FullSize') . ': <span class="badge text-bg-light border-radius-0">' . fm_get_filesize($all_files_size) . '</span>' ?>
-                            <?php echo lng('File') . ': <span class="badge text-bg-light border-radius-0">' . $num_files . '</span>' ?>
-                            <?php echo lng('Folder') . ': <span class="badge text-bg-light border-radius-0">' . $num_folders . '</span>' ?>
+                            <?php echo lng('Files') . ': <span class="badge text-bg-light border-radius-0">' . $num_files . '</span>' ?>
+                            <?php echo lng('Folders') . ': <span class="badge text-bg-light border-radius-0">' . $num_folders . '</span>' ?>
                         </td>
                     </tr>
                 </tfoot>
