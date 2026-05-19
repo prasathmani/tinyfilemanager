6c403a761ee36dfc833110550b6aae38273277eb
zedzhen <59135268+zedzhen@users.noreply.github.com>
2026-05-19 18:18:39 +0300

    add ace mode for .htaccess

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -160,6 +160,7 @@ $ext_language = array(
     'cs' => 'csharp',
     'ps1' => 'powershell',
     'htm' => 'html',
+    'htaccess' => 'apache_conf',
 );
 
 // if User has the external config file, try to use it to override the default config above [config.php]
