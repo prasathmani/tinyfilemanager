bbe111d8ffb78b63fdd4a7a147f21ee0049401cb
Slavomir Piar <33904511+slapiar@users.noreply.github.com>
2026-06-09 13:02:56 +0000

    Druhý krok k zjednoteniu triedy zobrazovania

--- src/renderers/main-page.php
+++ src/renderers/main-page.php
@@ -32,7 +32,7 @@
         <?php endif; ?>
     </div>
     <div class="table-responsive fm-table-wrap fm-list-clean-wrap">
-        <table class="table table-bordered table-hover table-sm align-middle fm-modern-table fm-list-clean" id="main-table" data-bs-theme="<?php echo FM_THEME; ?>">
+        <table class="table table-hover table-sm align-middle fm-modern-table fm-list-clean" id="main-table" data-bs-theme="<?php echo FM_THEME; ?>">
             <thead class="thead-white">
                 <tr>
                     <?php if (!FM_READONLY && !FM_UPLOAD_ONLY && FM_CAN_WRITE_IN_PATH): ?>
