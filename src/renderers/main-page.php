b76aa6ed8a6d6d597bb28b3afebaef2f9ac82509
Slavomir Piar <33904511+slapiar@users.noreply.github.com>
2026-06-11 04:18:13 +0000

    Vertikálne odsadenie sekcie tabuľky

--- src/renderers/main-page.php
+++ src/renderers/main-page.php
@@ -1,5 +1,4 @@
 <?php $all_files_size = 0; ?>
-<script src="src/assets/js/navbar-padding-fix.js?v=<?php echo rawurlencode((string) VERSION); ?>"></script>
 <form action="" method="post" class="pt-3 fm-shell">
     <input type="hidden" name="p" value="<?php echo fm_enc(FM_PATH) ?>">
     <input type="hidden" name="group" value="1">
