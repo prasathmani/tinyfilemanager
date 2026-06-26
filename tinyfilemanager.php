3d21aa149a44879492164658d09cfac00231377e
Caleb Mazalevskis <maikuolan@gmail.com>
2026-06-26 15:10:30 +0800

    Prevent redundant file reading.

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -2727,7 +2727,7 @@ function fm_is_exclude_items($name, $path)
 function fm_get_translations($tr)
 {
     try {
-        $content = @file_get_contents('translation.json');
+        static $content = @file_get_contents('translation.json');
         if ($content !== FALSE) {
             $lng = json_decode($content, TRUE);
             global $lang_list;
